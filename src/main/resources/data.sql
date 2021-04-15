f6b9bb6c932d187d0772ac9cfaf7b3bfce772378
Sara <sbr.90@hotmail.com>
2021-04-15 15:53:47 +0200

    final-project

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
-insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,tags)
+insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating)
  values('Alan', 'Sastre','172382193D', 'calle del pino,n 2, león','alan@sastre.com','6768292394',
-'15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'validado', '100',
-'[java, hibernate,spring]');
+'15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'validado', '100');
+
+insert into tags(name)values('java','hibernate','spring');
 
