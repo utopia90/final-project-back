7d7903907c13097cbed8ba16f5b2c07d0fe1ed25
Sara <sbr.90@hotmail.com>
2021-04-15 22:41:54 +0200

    final-project

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,62 @@
-insert into
- experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,availability)
+insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,availability)
  values('Alan', 'Sastre','172382193D', 'calle del pino,n 2, león','alan@sastre.com','6768292394',
 '15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'validado', '100','tardes');
+
+insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,availability)
+ values('Joaquín', 'Muñoz','293912393D', 'calle obispo 15, Madrid','joaquin@muñoz.com','6722292394',
+'15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'por validar', '55','mañanas');
+
+insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,availability)
+ values('María', 'Campos','12382193P', 'calle rozas,n 17, Barcelona','maria@campos.com','6398292394',
+'15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'validado', '95','tardes');
+
+insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,availability)
+ values('Carlos', 'Pérez','122390219S', 'calle matariles 12, cáceres','carlos@perez.com','671829294',
+'15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'por validar', '15','mañanas');
+
+insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,availability)
+ values('Marta', 'López','1122382193T', 'calle vieja,n 19, sevilla','marta@lopez.com','6928292394',
+'15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'validado', '100','tardes');
+
+insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,availability)
+ values('Alejandro', 'Sastre','12391239F', 'calle nueva,n 18, málaga','alejandro@sastre.com','6218292394',
+'15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'por validar', '65','mañanas');
+
+insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,availability)
+ values('Lourdes', 'Fernández','2934382193J', 'calle trafalgar,n 12, murcia','lourdes@fernandez.com','6328292394',
+'15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'validado', '82','mañanas');
+
+insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,availability)
+ values('Juan', 'Izquierdo','928382193S', 'calle platón 8, cádiz','izquierdo@juan.com','9238292394',
+'15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'validado', '100','tardes');
+
+insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,availability)
+ values('Roberto', 'Casales','212382193F', 'calle aristóteles 9, badajoz','casales@roberto.com','6128292332',
+'15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'por validar', '35','mañanas');
+
+insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,availability)
+ values('Rebeca', 'Amores','12338219J', 'calle plutón 2, extremadura','amores@rebeca.com','912292394',
+'15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'validado', '79','tardes');
+
+insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,availability)
+ values('Sofia', 'Rodriguez','192323823K', 'calle de la bajada 1, jaén','rodriguez@sofia.com','6128292394',
+'15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'validado', '79','mañanas');
+
+insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,availability)
+ values('Víctor', 'Robles','123382193D', 'calle hipatia,n 12, murcia','robles@victor.com','6128292394',
+'15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'validado', '100','mañanas');
+
+insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,availability)
+ values('Esteban', 'Campos','12382193Y', 'calle kafta 2, bilbao','campos@esteban.com','1238292394',
+'15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'por validar', '55','tardes');
+
+insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,availability)
+ values('Maria Angeles', 'Cobos','192382193R', 'calle copacabana 2, pontevedra','angeles@cobos.com','9348292394',
+'15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'validado', '68','tardes');
+
+insert into experts(name,surname,dni,address,mail,phone,created_at,updated_at,linkedln,state,rating,availability)
+ values('Juan José', 'Castaños','123122193S', 'calle izquierda,12','castaños@juan.com','673482212394',
+'15/04/2021', '16/05/2021','https://www.linkedin.com/in/sara-b-54b93543/', 'por validar', '98','mañanas');
 
 insert into tags(name)values('java');
 insert into tags(name)values('hibernate');
