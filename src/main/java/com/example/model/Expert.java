670cd00e6c00c5ee618006d896ce576d99767dbb
Sara <sbr.90@hotmail.com>
2021-04-18 02:42:28 +0200

    final-project

--- src/main/java/com/example/model/Expert.java
+++ src/main/java/com/example/model/Expert.java
@@ -30,7 +30,6 @@ public class Expert {
 
 
     @ManyToMany()
-    @JsonIgnore
     @JoinTable(
             name = "experts_tags",
             joinColumns = {@JoinColumn(name="tags_id", referencedColumnName = "id")},
