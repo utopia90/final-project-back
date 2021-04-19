83b1502982b0b01400889b92738aa117c6475bcf
Sara <sbr.90@hotmail.com>
2021-04-20 00:35:07 +0200

    final-project

--- src/main/java/com/example/model/Tag.java
+++ src/main/java/com/example/model/Tag.java
@@ -17,7 +17,7 @@ public class Tag {
     private Long id;
     private String name;
 
-    @ManyToMany(mappedBy="tags", cascade = {CascadeType.ALL})
+    @ManyToMany(mappedBy="tags")
     private List<Expert> experts = new ArrayList<>();
 
     public Tag() {
