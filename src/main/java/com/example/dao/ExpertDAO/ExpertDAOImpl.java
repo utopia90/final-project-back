4fd8ed9e06b333bcf7e2025265a66ee885c15bd8
Sara <sbr.90@hotmail.com>
2021-04-21 21:48:07 +0200

    final-project

--- src/main/java/com/example/dao/ExpertDAO/ExpertDAOImpl.java
+++ src/main/java/com/example/dao/ExpertDAO/ExpertDAOImpl.java
@@ -66,7 +66,7 @@ public class ExpertDAOImpl implements ExpertDAO {
       updatedExpert.setRating(expert.getRating());
       updatedExpert.setState(expert.getState());
 
-      updatedExpert.setTags(expert.getTags());
+      updatedExpert.getTags().addAll(expert.getTags());
       manager.merge(updatedExpert);
       return updatedExpert;
     }
