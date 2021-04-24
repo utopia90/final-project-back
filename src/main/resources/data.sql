349f1fead39670acd5d265547e1576e3ae340fd9
Sara <sbr.90@hotmail.com>
2021-04-24 12:44:39 +0200

    final-project

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,9 @@
-insert into experts (name, surname, dni, address,mail,phone,lindkedln, state,rating,availability, tags)
-values ('Alejandro', 'Ruiz', '9349234213D' 'calle ríos,31', 'alejandro@mail.com', '6282141940F', 'alejandro.linkedln.com','validado', '100', 'mañanas', 'php')
+INSERT INTO experts (name, surname, dni, address,mail,phone,lindkedln, state,rating,availability, tags)
+values ('Alejandro', 'Ruiz', '9349234213D' 'calle ríos,31', 'alejandro@mail.com', '6282141940F', 'alejandro.linkedln.com','validado', '100', 'mañanas', 'php';
 
+INSERT INTO experts (name, surname, dni, address,mail,phone,lindkedln, state,rating,availability, tags)
+values ('Borja', 'Fernández', '12318241234F', 'calle rivas,8', 'borja@mail.com', '2148249812G', 'borja.linkedln.com', 'por validar', '75', 'tardes', 'javascript');
 
-    insert into experts (name, surname, dni, address,mail,phone,lindkedln, state,rating,availability, tags)
-values ('Borja', 'Fernández', '12318241234F', 'calle rivas,8', 'borja@mail.com', '2148249812G', 'borja.linkedln.com', 'por validar', '75', 'tardes', 'javascript')
-
-insert into experts (name, surname, dni, address,mail,phone,lindkedln, state,rating,availability, tags)
-values ('María', 'Contreras', '13214912412X', 'calle nava,12', 'maria@mail.com', '12312415415F', 'maria.linkedln.com', '55', 'validado', 'mañanas', 'java')
+INSERT INTO experts (name, surname, dni, address,mail,phone,lindkedln, state,rating,availability, tags)
+values ('María', 'Contreras', '13214912412X', 'calle nava,12', 'maria@mail.com', '12312415415F', 'maria.linkedln.com', '55', 'validado', 'mañanas', 'java');
 
