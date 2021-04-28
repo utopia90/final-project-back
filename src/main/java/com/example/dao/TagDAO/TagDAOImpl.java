6603620d6a7083e69fc7f86dfe8c2abe25d5b14e
Sara <sbr.90@hotmail.com>
2021-04-28 11:03:33 +0200

    final-project

--- src/main/java/com/example/dao/TagDAO/TagDAOImpl.java
+++ src/main/java/com/example/dao/TagDAO/TagDAOImpl.java
@@ -60,7 +60,7 @@ public class TagDAOImpl implements TagDAO{
         if(id!=null){
 
 
-            manager.createNativeQuery("delete from experts_tags where tags_id ="+id);
+            manager.createNativeQuery("delete from experts_tags where tags_id like"+id);
 
 
         }
