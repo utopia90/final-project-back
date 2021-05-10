b6a64db1e36a3db9f75b65eee01290471177d64e
Sara <sbr.90@hotmail.com>
2021-05-10 18:29:58 +0200

    bu

---
+++ src/main/java/com/example/config/JavaMailSenderConfig.java
@@ -0,0 +1,32 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Properties;
+
+
+@Configuration
+@EnableWebMvc
+public class JavaMailSenderConfig{
+
+    @Bean
+    public JavaMailSenderImpl getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("boemiastore@gmail.com");
+        mailSender.setPassword("pinsula12");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
